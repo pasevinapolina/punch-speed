cd2c4f5363cd3edf5ad7211fb7c38b3bd068e4d3
Polina Pasevina <rigiemp@gmail.com>
2017-02-15 13:46:18 +0300

    Moved to data package

---
+++ app/src/main/java/com/artioml/practice/data/Result.java
@@ -0,0 +1,126 @@
+package com.artioml.practice.data;
+
+/**
+ * Created by Artiom L on 29.01.2017.
+ */
+
+public class Result {
+
+    private int punchType;
+    private String hand;
+    private String gloves;
+    private String glovesWeight;
+    private String position;
+    private float speed;
+    private float reaction;
+    private float acceleration;
+    private String date;
+    private String user;
+
+    public Result(int punchType, String hand, String gloves, String glovesWeight, String position,
+                  float speed, float reaction, float acceleration, String date) {
+        this.punchType = punchType;
+        this.hand = hand;
+        this.gloves = gloves;
+        this.glovesWeight = glovesWeight;
+        this.position = position;
+        this.speed = speed;
+        this.reaction = reaction;
+        this.acceleration = acceleration;
+        this.date = date;
+    }
+
+    public Result(String user, int punchType, String hand, String gloves, String glovesWeight, String position,
+                  float speed, float reaction, float acceleration, String date) {
+        this.punchType = punchType;
+        this.hand = hand;
+        this.gloves = gloves;
+        this.glovesWeight = glovesWeight;
+        this.position = position;
+        this.speed = speed;
+        this.reaction = reaction;
+        this.acceleration = acceleration;
+        this.date = date;
+        this.user = user;
+    }
+
+    public int getPunchType() {
+        return punchType;
+    }
+
+    public void setPunchType(int punchType) {
+        this.punchType = punchType;
+    }
+
+    public String getHand() {
+        return hand;
+    }
+
+    public void setHand(String hand) {
+        this.hand = hand;
+    }
+
+    public String getGloves() {
+        return gloves;
+    }
+
+    public void setGloves(String gloves) {
+        this.gloves = gloves;
+    }
+
+    public String getGlovesWeight() {
+        return glovesWeight;
+    }
+
+    public void setGlovesWeight(String glovesWeight) {
+        this.glovesWeight = glovesWeight;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public float getReaction() {
+        return reaction;
+    }
+
+    public void setReaction(float reaction) {
+        this.reaction = reaction;
+    }
+
+    public float getAcceleration() {
+        return acceleration;
+    }
+
+    public void setAcceleration(float acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
