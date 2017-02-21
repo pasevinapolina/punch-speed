a32b15f9bdae589a80c597c9cfcfb31cfb39ea4b
Polina Pasevina <rigiemp@gmail.com>
2017-02-21 13:14:27 +0300

    Settings dialog supports screen rotation

--- app/src/main/java/com/artioml/practice/activities/HistoryActivity.java
+++ app/src/main/java/com/artioml/practice/activities/HistoryActivity.java
@@ -1,7 +1,5 @@
 package com.artioml.practice.activities;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
@@ -14,23 +12,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
-import com.artioml.practice.data.DatabaseDescription;
 import com.artioml.practice.interfaces.SettingsChangeListener;
 import com.artioml.practice.interfaces.impl.HistorySettingsChangeListener;
 import com.artioml.practice.preferences.SettingsPreferenceManager;
-import com.artioml.practice.utils.HistoryListLoader;
+import com.artioml.practice.asynctasks.HistoryListLoader;
 import com.artioml.practice.views.ItemDivider;
 import com.artioml.practice.R;
 import com.artioml.practice.adapters.HistoryAdapter;
-import com.artioml.practice.data.HistoryDatabaseProvider;
-import com.artioml.practice.data.HistoryProvider;
 import com.artioml.practice.data.DatabaseDescription.History;
 import com.artioml.practice.models.Result;
 import com.artioml.practice.fragments.HistorySettingsDialog;
-import com.artioml.practice.inject.ServiceLocator;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -43,24 +36,16 @@ public class HistoryActivity extends AppCompatActivity
     private final static String TAG = HistoryActivity.class.getSimpleName();
 
     private static final String HISTORY_SETTINGS = "historySettings";
+    private static final String SETTINGS_DIALOG = "historySettingsDialog";
     private static final String _DESC = " DESC";
 
     public static final int LOADER_ID = 1;
-    private Loader<List<Result>> mLoader;
 
     private ArrayList<Result> history;
     private HistoryAdapter adapter;
     private RecyclerView recyclerView;
 
     private SettingsChangeListener settingsChangeListener;
-
-    //private SQLiteDatabase db;
-
-//    private String hand;
-//    private String gloves;
-//    private String position;
-//    private int punchType;
-//    private String sortOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +57,7 @@ public class HistoryActivity extends AppCompatActivity
         history = new ArrayList<>();
 
         settingsChangeListener = new HistorySettingsChangeListener(this, getWindow().getDecorView().getRootView());
-        fillSettingsPanel();
+        settingsChangeListener.fillSettingsPanel();
 
         recyclerView = (RecyclerView) findViewById(R.id.historyRecyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -81,7 +66,7 @@ public class HistoryActivity extends AppCompatActivity
         recyclerView.setAdapter(adapter);
         recyclerView.addItemDecoration(new ItemDivider(this));
 
-        mLoader = getSupportLoaderManager().initLoader(LOADER_ID, savedInstanceState, this);
+        getSupportLoaderManager().initLoader(LOADER_ID, savedInstanceState, this);
 
         //db = (PracticeDatabaseHelper.getInstance(this)).getReadableDatabase();
 
@@ -91,7 +76,7 @@ public class HistoryActivity extends AppCompatActivity
         settingsHistoryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                (new HistorySettingsDialog(HistoryActivity.this)).show();
+                (new HistorySettingsDialog()).show(getSupportFragmentManager(), SETTINGS_DIALOG);
             }
         });
 
@@ -99,9 +84,7 @@ public class HistoryActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_history, menu);
-        //selectedItem = 0;
         return true;
     }
 
@@ -116,7 +99,7 @@ public class HistoryActivity extends AppCompatActivity
                 preferenceManager.setSortOrderPreference(History.COLUMN_SPEED + _DESC);
                 break;
             case (R.id.reaction_sort):
-                preferenceManager.setSortOrderPreference(History.COLUMN_REACTION);
+                preferenceManager.setSortOrderPreference(History.COLUMN_REACTION + _DESC);
                 break;
             case (R.id.acceleration_sort):
                 preferenceManager.setSortOrderPreference(History.COLUMN_ACCELERATION + _DESC);
@@ -125,84 +108,24 @@ public class HistoryActivity extends AppCompatActivity
                 preferenceManager.setSortOrderPreference(History.COLUMN_DATE + _DESC);
                 break;
         }
-        mLoader = getSupportLoaderManager().restartLoader(LOADER_ID, null, this);
+        getSupportLoaderManager().restartLoader(LOADER_ID, null, this);
 
-        //sortOrder =
-        //historyProvider.setSortOrder(
-        //        sharedPreferences.getString(SORT_ORDER, History.COLUMN_DATE + _DESC));
-        //initHistory();
-        /*if (item.getItemId() == R.id.speed_sort ||
-                item.getItemId() == R.id.reaction_sort ||
-                item.getItemId() == R.id.acceleration_sort ||
-                item.getItemId() == R.id.date_sort) {
-            if (item.isChecked()) {
-                Drawable drawable = item.getIcon();
-                drawable.clearColorFilter();
-                item.setIcon(drawable);
-                item.setIcon(drawable);
-                item.setChecked(false);
-            }
-        }*//*} else {
-                Drawable drawable = item.getIcon();
-                int color = ContextCompat.getColor(this, R.color.colorGreyDark);
-                drawable.setColorFilter(color, PorterDuff.Mode.SRC_ATOP);
-                drawable = DrawableCompat.wrap(drawable);
-                item.setIcon(drawable);
-                item.setChecked(true);
-            }*/
-        /*switch (item.getItemId()) {
-            case R.id.vibrate:
-            case R.id.dont_vibrate:
-                if (item.isChecked()) item.setChecked(false);
-                else item.setChecked(true);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }*/
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     protected void onResume() {
         Log.i(TAG, "onResume");
-        fillSettingsPanel();
+        settingsChangeListener.fillSettingsPanel();
         //initHistory();
-        mLoader = getSupportLoaderManager().restartLoader(LOADER_ID, null, this);
+        getSupportLoaderManager().restartLoader(LOADER_ID, null, this);
         super.onResume();
     }
 
     public void updateSettings() {
-        fillSettingsPanel();
-        //initHistory();
-        mLoader = getSupportLoaderManager().restartLoader(LOADER_ID, null, this);
-    }
-
-//    private void initHistory () {
-//
-//        historyProvider.clearHistory();
-//        historyProvider.addDataSet();
-//
-//        history.clear();
-//        history.addAll(historyProvider.getHistoryList());
-//
-//        adapter.notifyDataSetChanged();
-//    }
-
-    private void fillSettingsPanel(){
-        SharedPreferences sharedPreferences =
-                getSharedPreferences(HISTORY_SETTINGS, Context.MODE_PRIVATE);
-
-        //historyProvider.setSortOrder(
-          //      sharedPreferences.getString(SORT_ORDER, DatabaseDescription.History.COLUMN_DATE + _DESC));
-
         settingsChangeListener.fillSettingsPanel();
-        //Result settings = ((HistorySettingsChangeListener)settingsChangeListener).getCurrentSettings();
-
-        //if(historyProvider instanceof HistoryDatabaseProvider) {
-//            ((HistoryDatabaseProvider) historyProvider)
-//                    .setPunchParameters(settings.getPunchType(), settings.getHand(),
-//                            settings.getGloves(), settings.getPosition());
-//        }
+        //initHistory();
+        getSupportLoaderManager().restartLoader(LOADER_ID, null, this);
     }
 
     @Override
